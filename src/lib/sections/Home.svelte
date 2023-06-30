a09277f4e0f2328774259f70918b60329c8b567a
verdulife <verdu@live.com>
2023-06-30 20:16:32 +0200

    tech start

--- src/lib/sections/Home.svelte
+++ src/lib/sections/Home.svelte
@@ -2,12 +2,12 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		const wrapper = document.querySelector('.wrapper');
+		const imagesWrapper = document.querySelector('.imagesWrapper');
 		const images = [];
 
 		for (let i = 0; i < 120; i++) {
 			images[i] = document.createElement('img');
-			wrapper.appendChild(images[i]);
+			imagesWrapper.appendChild(images[i]);
 			images[i].src = `/h2/${i + 1}.png`;
 			images[i].style.cssText = `
 				position: absolute;
@@ -31,70 +31,135 @@
 	});
 </script>
 
-<section id="home" class="row fcenter full">
-	<article class="col w1/2">
-		<h1><b>La cintura <br /> ergonomica</b></h1>
-		<p><strong>PRESENTADO <span>WAISTLINE_2.0</span> TECHNOLOGY</strong></p>
+<section id="home" class="full">
+	<div class="wrapper row fcenter full">
+		<article class="col w1/2">
+			<h1><b>muevete <br /> <i>libremente</i></b></h1>
+			<p><strong>PRESENTANDO <span>SIMPLE-MENTE</span> TECH 2.0</strong></p>
 
-		<p>
-			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, animi voluptatibus
-			mollitia facilis facere incidunt ea sunt. Facilis cumque nam obcaecati vel ipsum vero
-			cupiditate tempora batae.
-		</p>
+			<p>
+				Un pantalón práctico, de carácter minimalista ideal para todas aquellas personas que
+				aprecian sentirse cómodos, en todo momento, que les gusta moverse sin limitaciones y
+				prefieren calidad frente a cantidad.
+			</p>
 
-		<footer class="row acenter">
-			<a role="button" href="/#tech">SABER MÁS</a>
-			<a role="button" class="unset" href="/#contact">CONTACTO</a>
-		</footer>
-	</article>
+			<footer class="row acenter">
+				<a role="button" href="/#tech">SABER MÁS</a>
+				<a role="button" class="unset" href="/#contact">CONTACTO</a>
+			</footer>
+		</article>
 
-	<aside class="wrapper row w1/2 hfull" />
+		<aside class="imagesWrapper row w1/2 hfull" />
+	</div>
+</section>
+
+<section id="endless" class="full">
+	<div class="wrapper row fcenter full">
+		<article class="col w1/2">
+			<h1><b>Cintura ergonómica</b></h1>
+			<p><strong>descubre un nuevo <span>movimiento</span></strong></p>
+
+			<p>
+				Nuetro diseño soluciona de manera sencilla y elegante el ajuste correcto y ergonómico a toda
+				cintura.
+				<br /><br />
+				<span>Deja de sentir las incomodas limitaciones de los pantalones actúeles</span>
+				<br /><br />
+				Liberar nuestro centro energético de manera que la respiración y nuestro movimiento natural no
+				se vean afectados con un ajuste preciso es una sensación tan agradable que después de probarte
+				unos, simpleMENTE te será difícil imaginar que unos pantalones descuiden este propósito.
+			</p>
+		</article>
+
+		<aside class="wrapper row w1/2 hfull" />
+	</div>
 </section>
 
 <style lang="postcss">
-	section {
+	.wrapper {
 		max-width: var(--media-xl);
 		margin: 0 auto;
+	}
+
+	#home {
+		& h1 {
+			color: var(--alt);
+			font-size: 100px;
+			line-height: 1;
+
+			& i {
+				color: var(--accent);
+			}
+		}
+
+		& strong {
+			position: relative;
+			font-family: var(--font-code);
+
+			& span {
+				color: var(--accent);
+			}
+
+			&:before {
+				content: '';
+				position: absolute;
+				bottom: -1em;
+				right: -1em;
+				width: 1000px;
+				height: 1px;
+				background-color: var(--alt);
+			}
+		}
+
+		& p {
+			max-width: 600px;
+		}
+
+		& a[role='button'] {
+			width: 200px;
+		}
+	}
+
+	#endless {
+		background-color: var(--alt);
+
+		& h1 {
+			color: var(--base);
+			font-size: 100px;
+			line-height: 1;
+		}
+
+		& strong {
+			position: relative;
+			font-family: var(--font-code);
+			color: var(--base);
+
+			& span {
+				color: var(--accent);
+			}
+
+			&:before {
+				content: '';
+				position: absolute;
+				bottom: -1em;
+				right: -1em;
+				width: 1000px;
+				height: 1px;
+				background-color: var(--alt);
+			}
+		}
+
+		& p {
+			max-width: 600px;
+			color: var(--base);
+		}
 	}
 
 	article {
 		gap: 2em;
 	}
 
-	h1 {
-		color: var(--alt);
-		font-size: 100px;
-		line-height: 1;
-	}
-
-	strong {
-		position: relative;
-		font-family: var(--font-code);
-
-		& span {
-			color: var(--accent);
-		}
-
-		&:before {
-			content: '';
-			position: absolute;
-			bottom: -1em;
-			right: -1em;
-			width: 1000px;
-			height: 1px;
-			background-color: var(--alt);
-		}
-	}
-
-	p {
-		max-width: 600px;
-	}
-
-	a[role='button'] {
-		width: 200px;
-	}
-
-	.wrapper {
+	.imagesWrapper {
 		position: relative;
 	}
 </style>
--- src/lib/sections/Tech.svelte
+++ src/lib/sections/Tech.svelte
@@ -1,17 +1,54 @@
+<script>
+	const tabs = [
+		{
+			title: 'Ajuste preciso',
+			desc: 'El ancho de la zona abdominal sujeta sin sentir ningún tipo de presión en esta zona pues el ajuste se da en las crestas iliacas por un cordón de tela que pasa por unas clavillas situadas a los costados, el sobrante de la tela se dobla en forma de S entre la cresta iliaca y el abdomen sin molestia ninguna, una considerable altura en la zona lumbar hace el resto, que la tela sostenga en vez sentir presión, sintiendo un agradable abrazo que sujeta el pantalón a nuestro torso.',
+			image1: '',
+			image2: '',
+			image3: ''
+		},
+		{
+			title: 'Entrepierna',
+			desc: 'El ancho de la zona abdominal sujeta sin sentir ningún tipo de presión en esta zona pues el ajuste se da en las crestas iliacas por un cordón de tela que pasa por unas clavillas situadas a los costados, el sobrante de la tela se dobla en forma de S entre la cresta iliaca y el abdomen sin molestia ninguna, una considerable altura en la zona lumbar hace el resto, que la tela sostenga en vez sentir presión, sintiendo un agradable abrazo que sujeta el pantalón a nuestro torso.',
+			image1: '',
+			image2: '',
+			image3: ''
+		},
+		{
+			title: 'Tobillo',
+			desc: 'El ancho de la zona abdominal sujeta sin sentir ningún tipo de presión en esta zona pues el ajuste se da en las crestas iliacas por un cordón de tela que pasa por unas clavillas situadas a los costados, el sobrante de la tela se dobla en forma de S entre la cresta iliaca y el abdomen sin molestia ninguna, una considerable altura en la zona lumbar hace el resto, que la tela sostenga en vez sentir presión, sintiendo un agradable abrazo que sujeta el pantalón a nuestro torso.',
+			image1: '',
+			image2: '',
+			image3: ''
+		}
+	];
+
+	$: tabIndex = 0;
+	$: tab = tabs[tabIndex];
+</script>
+
 <section id="tech" class="row fcenter full">
 	<article class="col w1/2">
-		<h1><b>La cintura <br /> ergonomica</b></h1>
-		<p><strong>PRESENTADO <span>WAISTLINE_2.0</span> TECHNOLOGY</strong></p>
+		<ul class="row">
+			<li>
+				<button on:click={() => (tabIndex = 0)}>CADERA</button>
+			</li>
+			<li>
+				<button on:click={() => (tabIndex = 1)}>ENTRPIERNA</button>
+			</li>
+			<li>
+				<button on:click={() => (tabIndex = 2)}>TOBILLO</button>
+			</li>
+		</ul>
 
-		<p>
-			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, animi voluptatibus
-			mollitia facilis facere incidunt ea sunt. Facilis cumque nam obcaecati vel ipsum vero
-			cupiditate tempora batae.
-		</p>
+		<main>
+			<h2>{tab.title}</h2>
+			<p>{tab.desc}</p>
+		</main>
 
-		<footer class="row acenter">
-			<a role="button" href="/#tech">SABER MÁS</a>
-			<a role="button" class="unset" href="/#contact">CONTACTO</a>
+		<footer class="row">
+			<img src={tab.image1} alt="image 1" />
+			<img src={tab.image2} alt="image 2" />
 		</footer>
 	</article>
 
@@ -20,7 +57,8 @@
 
 <style lang="postcss">
 	section {
-		background-color: var(--alt);
+		max-width: var(--media-xl);
+		margin: 0 auto;
 	}
 
 	article {
