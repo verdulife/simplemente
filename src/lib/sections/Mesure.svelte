2da2a0398646a4eccb2b314e17b2991a410cb19c
verdulife <verdu@live.com>
2023-07-06 16:28:32 +0200

    video

---
+++ src/lib/sections/Mesure.svelte
@@ -0,0 +1,30 @@
+<script>
+	let videoState = true;
+	let volume = 1;
+
+	function playPause() {
+		videoState = !videoState;
+	}
+</script>
+
+<section class="row fcenter full">
+	<article class="video-wrapper">
+		<video
+			class="full"
+			src="/medicion.mp4"
+			playsinline
+			bind:paused={videoState}
+			bind:volume
+			on:click={playPause}
+		/>
+	</article>
+</section>
+
+<style lang="postcss">
+	article {
+		width: 300px;
+		aspect-ratio: 9/14;
+		border-radius: 1em;
+		overflow: hidden;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
 	import Meta from '$components/global/Meta.svelte';
 
 	import Home from '$sections/Home.svelte';
-	import Tech from '$sections/Tech.svelte';
+	import Mesure from '$sections/Mesure.svelte';
+	/* import Tech from '$sections/Tech.svelte'; */
 </script>
 
 <!-- <Meta data={home} /> -->
 
 <Home />
-<Tech />
+<Mesure />
+<!-- <Tech /> -->
