45967daf0f2ba292b56f0803cea2deb7b556e995
verdulife <verdu@live.com>
2023-07-18 19:18:36 +0200

    til metadata 2

--- src/lib/components/global/Meta.svelte
+++ src/lib/components/global/Meta.svelte
@@ -12,12 +12,12 @@
 	<meta property="og:url" content={data.url} />
 	<meta property="og:title" content={data.title} />
 	<meta property="og:description" content={data.description} />
-	<meta property="og:image" content={data.image} />
-	<meta property="og:image:secure_url" content={data.image} />
-	<meta property="og:image:type" content="image/jpeg" />
+	<meta property="og:image" content={data.card_image} />
+	<meta property="og:image:secure_url" content={data.card_image} />
+	<meta property="og:image:type" content="image/png" />
 
 	<!-- Twitter -->
-	<meta name="twitter:card" content="{data.card_image}" />
+	<meta name="twitter:card" content={data.card_image} />
 	<meta name="twitter:site" content={data.url} />
 	<meta name="twitter:title" content={data.title} />
 	<meta name="twitter:description" content={data.description} />
